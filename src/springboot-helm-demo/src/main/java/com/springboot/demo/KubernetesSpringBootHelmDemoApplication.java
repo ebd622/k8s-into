c4ad76ef798e7669a3e899db93aed290509c9df5
Eugeny <eburdo@gmail.com>
2021-02-14 12:55:08 +0100

    change description

--- src/springboot-helm-demo/src/main/java/com/springboot/demo/KubernetesSpringBootHelmDemoApplication.java
+++ src/springboot-helm-demo/src/main/java/com/springboot/demo/KubernetesSpringBootHelmDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KubernetesSpringBootHelpDemoApplication {
+public class KubernetesSpringBootHelmDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KubernetesSpringBootHelpDemoApplication.class, args);
+		SpringApplication.run(KubernetesSpringBootHelmDemoApplication.class, args);
 	}
 
 }
